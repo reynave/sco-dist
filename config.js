e712adba41e42f48ab78262a85f828e4dc0c5ea1
reynave <felix.renaldi@gmail.com>
2023-04-28 15:55:18 +0700

    update

---
+++ config.js
@@ -0,0 +1,5 @@
+var env_api = "http://194.233.77.27/cso/api/";
+var env_public = ""; 
+var env_base_url ="http://194.233.77.27/cso/admin/#/";
+var env_socket = 'http://194.233.77.27:3000';
+var env_terminal1 = ' "http://194.233.77.27/cso/terminal1/#/"';
